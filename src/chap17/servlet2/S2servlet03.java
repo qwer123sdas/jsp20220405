f9ec5eb8441e6a73399d51b1256eb8f2ce5a09e9
hyunsu <violin0117@naver.com>
2022-04-19 12:40:57 +0900

    goPost()메소드 사용하기 연습

---
+++ src/chap17/servlet2/S2servlet03.java
@@ -0,0 +1,46 @@
+package chap17.servlet2;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S2servlet03
+ */
+@WebServlet("/S2servlet03")
+public class S2servlet03 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public S2servlet03() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+		String ageStr = request.getParameter("age");
+		
+		System.out.println(email);
+		System.out.println(address);
+		System.out.println(ageStr);
+	}
+
+}
