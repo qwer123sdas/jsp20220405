f42eff100fb0476ac17127e84eabf6541aae4482
hyunsu <violin0117@naver.com>
2022-04-21 17:17:00 +0900

    db read

--- src/chap14/S14Servlet01.java
+++ src/chap14/S14Servlet01.java
@@ -1,13 +1,18 @@
 package chap14;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.*;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class S14Servlet01
@@ -29,10 +34,45 @@ public class S14Servlet01 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// database에서 records 가져오기
+		/*
 		List<String> cities = new ArrayList<>();
 		cities.add("Berlin");
 		cities.add("London");
 		cities.add("Madrid");
+		*/
+		
+		ServletContext application = getServletContext();
+		List<String> cities = new ArrayList<>();
+		
+		// 1. 연결설정
+		DataSource ds = (DataSource)application.getAttribute("dbpool");
+		
+		String sql ="SELECT city FROM Customers";
+		try(
+			Connection con = ds.getConnection();
+			
+			// 2. statement 객체생성
+			Statement stmt = con.createStatement();
+			
+			// 3. 쿼리 실행 = sqld을 실행
+			ResultSet rs = stmt.executeQuery(sql); // 명령문을 실행
+		){
+			
+			// 4. 실행결과 정제
+			while(rs.next()) {
+				String city = rs.getString(1);
+				/*System.out.println(city);*/
+				cities.add(city);
+				
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		// 5. 자원 닫기
+		/*rs.close(); stmt.close(); */
+		
+		
+		
 		
 		// request에 records ㄴ넣기
 		request.setAttribute("cities", cities);
