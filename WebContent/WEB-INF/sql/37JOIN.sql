fe2ec43a3cb49600d2c4676dc19d15b46bc108ff
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-05-03 11:43:56 +0900

    Update 37JOIN.sql

---
+++ WebContent/WEB-INF/sql/37JOIN.sql
@@ -0,0 +1,17 @@
+USE w3schools;
+
+DESC Orders;
+DESC Customers;
+-- Orders : 주문, Costomers : 고객
+--  주문한 적 없는 고객명 조회
+SELECT c.CustomerName, o.* FROM Customers c LEFT JOIN Orders o ON o.CustomerID = c.CustomerID
+WHERE o.OrderID IS NULL
+ORDER BY 1;
+
+-- 고객별 주문 건수를 0건도 나오도록 하기
+SELECT c.CustomerName, COUNT(o.OrderID) count 
+FROM Customers c LEFT JOIN Orders o 
+ON o.CustomerID = c.CustomerID
+group by c.CustomerName
+ORDER BY 2 DESC;
+
