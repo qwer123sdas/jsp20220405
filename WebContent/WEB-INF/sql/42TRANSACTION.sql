077950f0349b17763dbdcdeb05e20b1b976639e9
hyunsu <violin0117@naver.com>
2022-05-04 11:52:03 +0900

    JOIN 3개 이상

---
+++ WebContent/WEB-INF/sql/42TRANSACTION.sql
@@ -0,0 +1,16 @@
+USE mydb2;
+
+DELETE FROM Reply WHERE board_id = 14;
+
+ROLLBACK;
+
+DELETE FROM Board
+WHERE id = 14;
+
+COMMIT;
+
+SELECT * FROM Reply WHERE board_id = 14;
+
+-- DML(INSERT UPDATE DELETE)
+
+
---
+++ WebContent/WEB-INF/sql/43JOIN.sql
@@ -0,0 +1,44 @@
+USE w3schools;
+DESC Orders;
+
+SELECT * FROM Orders;
+-- 1996년 7월 4일에 주문한 고객 이름 찾기
+SELECT o.OrderDate, c.CustomerName, o.EmployeeID
+FROM Orders o 
+JOIN Customers c 
+ON o.CustomerID = c.CustomerID
+WHERE o.OrderDate = '1996-07-04';
+
+-- 1996.07.04에 주문한 고객의 이름과 처리한 직원 이름
+-- concat : ????
+; 
+
+-- 1996.7.4의 주SELECT o.OrderDate, c.CustomerName, concat(e.FirstName, ' ', e.LastName) EmployeeName
+FROM Orders o JOIN Customers c ON o.CustomerID = c.CustomerID
+				JOIN Employees e ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderDate = '1996-07-04'문을 처리한 직원명과 배송자 명 조회
+SELECT o.OrderDate, concat(e.FirstName, ' ', e.LastName) EmployeeName, s.ShipperName
+FROM Orders o JOIN Employees e ON o.EmployeeID = e.EmployeeID
+				JOIN Shippers s ON o.ShipperID = s.ShipperID
+WHERE o.OrderDate = '1996-07-04';
+
+-- 1996.7.4의 주문한 고객과, 처리한 직원과 배송한 배송자 명
+SELECT o.OrderDate, c.CustomerName, concat(e.FirstName, ' ', e.LastName) EmployeeName, s.ShipperName
+FROM Orders o JOIN Employees e ON o.EmployeeID = e.EmployeeID
+				JOIN Shippers s ON o.ShipperID = s.ShipperID
+                JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE o.OrderDate = '1996-07-08';
+
+-- 1996-07-04에 주문한 상품명(들)
+SELECT o.OrderDate, p.ProductName, od.Quantity, p.Price, (od.Quantity * p.Price) total
+FROM OrderDetails od JOIN Orders o ON o.OrderID = od.OrderID
+				JOIN Products p ON od.ProductID = p.ProductID
+WHERE o.OrderDate = '1996-07-04';
+
+-- 1996-07-08에 각 고객의 주문 총액
+SELECT o.OrderDate, c.CustomerName, SUM(od.Quantity * p.Price) total
+FROM OrderDetails od JOIN Products p ON od.ProductID = p.ProductID
+					JOIN Orders o ON o.OrderID = od.OrderID
+					JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE OrderDate = '1996-07-08'
+GROUP BY c.CustomerName;
